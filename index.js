df93f26ec460cc131213a3c2126e66b5754f17a2
lelik <seomasterweb@gmail.com>
2018-01-29 16:04:52 +0200

    tabs in swagger substituted with spaces

--- index.js
+++ index.js
@@ -8,3 +8,11 @@
      headers: {'Content-Type': 'application/json'}
    });
  };
+exports.get2 = function(event, context) {
+	console.log(event, 'logggggg2');
+	context.succeed({
+		statusCode: 200,
+		body: JSON.stringify({response: 'success2'}),
+		headers: {'Content-Type': 'application/json'}
+	});
+};
